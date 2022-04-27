31b58df5f5db6b8152fda627e4935c24a3091e62
yafira <yafira.martinez@lc.cuny.edu>
2022-04-26 20:38:15 -0400

    setup outlet collections for UI elements

--- apple-pie/ViewController.swift
+++ apple-pie/ViewController.swift
@@ -9,11 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var treeImageView: UIImageView!
+    @IBOutlet weak var correctWordLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    @IBOutlet var buttons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+    }
+    
 
 }
 
